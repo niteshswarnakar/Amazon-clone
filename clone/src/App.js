6594a4d9b45c7c076b785f97ab1ce6b7678acf6e
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-01-04 16:02:01 +0545

    removed login session

--- clone/src/App.js
+++ clone/src/App.js
@@ -3,47 +3,47 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
-// import Checkout from "./Checkout";
-// import Login from "./Login";
+import Checkout from "./Checkout";
+import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function App() {
-  const [{ user }, dispatch] = useStateValue();
+  // const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
-    const unsubscribe = auth?.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
-        });
-      } else {
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
-      }
-    });
-    return () => {
-      unsubscribe();
-    };
+    // const unsubscribe = auth?.onAuthStateChanged((authUser) => {
+    //   if (authUser) {
+    //     dispatch({
+    //       type: "SET_USER",
+    //       user: authUser,
+    //     });
+    //   } else {
+    //     dispatch({
+    //       type: "SET_USER",
+    //       user: null,
+    //     });
+    //   }
+    // });
+    // return () => {
+    //   unsubscribe();
+    // };
   });
 
-  console.log(user);
+  // console.log(user);
 
   return (
     <Router>
       <div className="app">
         <Switch>
-          {/* <Route path="/login">
+          <Route path="/login">
             <Login />
           </Route>
 
           <Route path="/checkout">
             <Header />
             <Checkout />
-          </Route> */}
+          </Route>
 
           <Route path="/">
             <Header />
